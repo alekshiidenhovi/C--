dc84968d9f0c4e8e8037231487d965151120142f
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-12-08 22:07:07 +0200

    impl TokenType, conversion from Token to TokenType and displaying

--- src/compiler/tokens/mod.rs
+++ src/compiler/tokens/mod.rs
@@ -1,5 +1,7 @@
+use std::fmt;
+
 /// Represents a token in the C language.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
     Identifier(String),
     Constant(i32),
@@ -11,4 +13,91 @@ pub enum Token {
     OpenBrace,
     CloseBrace,
     Semicolon,
+}
+
+impl Token {
+    /// Returns the `TokenType` variant of the `Token`.
+    ///
+    /// This function maps the different `Token` enum variants to their corresponding
+    /// `TokenType` enum variants, providing a simple way to categorize tokens.
+    ///
+    /// # Returns
+    ///
+    /// The `TokenType` that represents the kind of the token.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use cmm::tokens::Token;
+    /// # use cmm::tokens::TokenType;
+    ///
+    /// let identifier_token = Token::Identifier(String::from("variable"));
+    /// assert_eq!(identifier_token.kind(), TokenType::Identifier);
+    ///
+    /// let int_keyword_token = Token::IntKeyword;
+    /// assert_eq!(int_keyword_token.kind(), TokenType::IntKeyword);
+    /// ```
+    pub fn kind(&self) -> TokenType {
+        match self {
+            Token::Identifier(_) => TokenType::Identifier,
+            Token::Constant(_) => TokenType::Constant,
+            Token::IntKeyword => TokenType::IntKeyword,
+            Token::VoidKeyword => TokenType::VoidKeyword,
+            Token::ReturnKeyword => TokenType::ReturnKeyword,
+            Token::OpenParen => TokenType::OpenParen,
+            Token::CloseParen => TokenType::CloseParen,
+            Token::OpenBrace => TokenType::OpenBrace,
+            Token::CloseBrace => TokenType::CloseBrace,
+            Token::Semicolon => TokenType::Semicolon,
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Token::Identifier(identifier) => write!(f, "Identifier: {}", identifier),
+            Token::Constant(constant) => write!(f, "Constant: {}", constant),
+            Token::IntKeyword => write!(f, "IntKeyword"),
+            Token::VoidKeyword => write!(f, "VoidKeyword"),
+            Token::ReturnKeyword => write!(f, "ReturnKeyword"),
+            Token::OpenParen => write!(f, "OpenParen"),
+            Token::CloseParen => write!(f, "CloseParen"),
+            Token::OpenBrace => write!(f, "OpenBrace"),
+            Token::CloseBrace => write!(f, "CloseBrace"),
+            Token::Semicolon => write!(f, "Semicolon"),
+        }
+    }
+}
+
+/// Represents the type of a token.
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokenType {
+    Identifier,
+    Constant,
+    IntKeyword,
+    VoidKeyword,
+    ReturnKeyword,
+    OpenParen,
+    CloseParen,
+    OpenBrace,
+    CloseBrace,
+    Semicolon,
+}
+
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TokenType::Identifier => write!(f, "Identifier"),
+            TokenType::Constant => write!(f, "Constant"),
+            TokenType::IntKeyword => write!(f, "IntKeyword"),
+            TokenType::VoidKeyword => write!(f, "VoidKeyword"),
+            TokenType::ReturnKeyword => write!(f, "ReturnKeyword"),
+            TokenType::OpenParen => write!(f, "OpenParen"),
+            TokenType::CloseParen => write!(f, "CloseParen"),
+            TokenType::OpenBrace => write!(f, "OpenBrace"),
+            TokenType::CloseBrace => write!(f, "CloseBrace"),
+            TokenType::Semicolon => write!(f, "Semicolon"),
+        }
+    }
 }
