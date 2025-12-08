137745580f681624c6e818d012191e8fe7115da4
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-12-08 22:39:35 +0200

    add comparison support for ast enums

--- src/compiler/parser/ast.rs
+++ src/compiler/parser/ast.rs
@@ -1,26 +1,26 @@
 /// Represents the abstract syntax tree of a program.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Ast {
     /// A program is composed of a single function.
     Program(FunctionDefinition),
 }
 
 /// Represents a function definition.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum FunctionDefinition {
     /// A function definition consisting of its name and body.
     Function(String, Statement),
 }
 
 /// Represents a statement within a function.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Statement {
     /// A return statement, which returns an expression.
     Return(Expression),
 }
 
 /// Represents an expression that evaluates to a value.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Expression {
     /// Represents an integer literal constant.
     IntegerConstant(i32),
