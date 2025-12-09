452e797193a190d3ed68de1f0b2c642a52453e39
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-12-09 15:22:59 +0200

    add debug and partialeq traits to assembly ast nodes

--- src/compiler/codegen/asm_ast.rs
+++ src/compiler/codegen/asm_ast.rs
@@ -1,16 +1,19 @@
 /// Represents an abstract syntax tree for assembly code.
+#[derive(Debug, PartialEq)]
 pub enum AssemblyAst {
     /// Represents a complete program, containing a single function definition.
     Program(FunctionDefinition),
 }
 
 /// Represents the definition of a function.
+#[derive(Debug, PartialEq)]
 pub enum FunctionDefinition {
     /// A function with a name and a list of instructions.
     Function(String, Vec<Instruction>),
 }
 
 /// Represents a single instruction in the assembly code.
+#[derive(Debug, PartialEq)]
 pub enum Instruction {
     /// Move operation: copies a value from a source operand to a destination operand.
     Mov(Operand, Operand),
@@ -19,6 +22,7 @@ pub enum Instruction {
 }
 
 /// Represents an operand for an instruction, which can be an immediate value or a register.
+#[derive(Debug, PartialEq)]
 pub enum Operand {
     /// An immediate integer value.
     Imm(i32),
