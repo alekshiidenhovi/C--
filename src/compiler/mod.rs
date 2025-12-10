767add5a98980aad3206f398c9890a21a5972c3b
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-12-10 18:34:48 +0200

    add docstring to compiler orchestrator

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -7,13 +7,33 @@ pub mod tokens;
 use parser::Parser;
 use std::path::Path;
 
-/// Represents the different stages of a compilation process.
+/// Represents the different stages a C-- compilation can proceed to.
+///
+/// This enum allows for early termination of the compilation process after a specific stage.
 pub enum Stage {
+    /// Stop after the lexing stage.
     Lex,
+    /// Stop after the parsing stage.
     Parse,
+    /// Stop after the code generation stage.
     Codegen,
 }
 
+/// Compiles a preprocessed C-- source file to assembly code.
+///
+/// This function orchestrates the entire compilation pipeline, from lexing to assembly emission.
+/// It can be configured to stop at a specific stage using the `process_until` argument.
+///
+/// # Arguments
+///
+/// * `input_path`: The path to the C-- source file to compile.
+/// * `output_path`: The path where the generated assembly code will be written.
+/// * `process_until`: An optional `Stage` to specify the maximum compilation stage to reach.
+///
+/// # Returns
+///
+/// Returns `Ok(())` on successful compilation, or an `anyhow::Error` if any stage of the
+/// compilation fails.
 pub fn run_cmm_compiler(
     input_path: &Path,
     output_path: &Path,
