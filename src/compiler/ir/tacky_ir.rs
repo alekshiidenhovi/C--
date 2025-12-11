22bad567a62d3b303c67952a0b84d7881ac5e8f6
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-12-11 11:34:16 +0200

    add tacky ir ast

---
+++ src/compiler/ir/tacky_ir.rs
@@ -0,0 +1,52 @@
+/// Represents the top-level structure of TACKY Intermediate Representation.
+#[derive(Debug, Clone, PartialEq)]
+pub enum TackyIR {
+    /// A complete TACKY function definition.
+    Program(TackyFunction),
+}
+
+/// Represents a TACKY function definition.
+#[derive(Debug, Clone, PartialEq)]
+pub enum TackyFunction {
+    /// Defines a function with a unique identifier and a list of instructions.
+    Function {
+        /// The unique name of the function.
+        identifier: String,
+        /// The sequence of instructions that make up the function's body.
+        instructions: Vec<TackyInstruction>,
+    },
+}
+
+/// Represents a single TACKY instruction.
+#[derive(Debug, Clone, PartialEq)]
+pub enum TackyInstruction {
+    /// An instruction that returns a value from the function.
+    Return(TackyValue),
+    /// An instruction that performs a unary operation on a value.
+    Unary {
+        /// The unary operator to be applied.
+        operator: TackyUnaryOperator,
+        /// The source value for the operation.
+        source: TackyValue,
+        /// The destination where the result of the operation will be stored.
+        destination: TackyValue,
+    },
+}
+
+/// Represents a value within the TACKY IR.
+#[derive(Debug, Clone, PartialEq)]
+pub enum TackyValue {
+    /// Represents an integer constant.
+    Constant(i32),
+    /// Represents a variable, identified by its name.
+    Variable(String),
+}
+
+///  Represents a unary operator within the TACKY IR.
+#[derive(Debug, Clone, PartialEq)]
+pub enum TackyUnaryOperator {
+    /// Bitwise complement
+    Complement,
+    /// Arithmetic negation
+    Negate,
+}
