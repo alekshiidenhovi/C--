a0b8e17cb897a1b259a6faaf64a71281274b1e0f
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-12-17 14:29:09 +0200

    refactor code emission step

--- src/compiler/code_emission/mod.rs
+++ src/compiler/code_emission/mod.rs
@@ -34,12 +34,12 @@ fn emit_function(function: &AssemblyFunction) -> String {
             instructions,
         } => {
             let asm_identifier = "_".to_string() + identifier;
-            let mut function_code = format!("\t.globl {}\n", asm_identifier);
-            function_code.push_str(&format!("{}:\n", asm_identifier));
-            function_code.push_str(&format!("\tpushq %rbp\n"));
-            function_code.push_str(&format!("\tmovq %rsp, %rbp\n"));
+            let mut function_code = wrap_instruction(format!(".globl {}", asm_identifier).as_str());
+            function_code.push_str(&wrap_label(asm_identifier.as_str()));
+            let prologue = wrap_instruction("pushq %rbp") + &wrap_instruction("movq %rsp, %rbp");
+            function_code.push_str(&prologue);
             for instruction in instructions {
-                function_code.push_str(&format!("{}\n", emit_instruction(instruction)));
+                function_code.push_str(&format_instruction(instruction));
             }
             function_code
         }
@@ -55,27 +55,30 @@ fn emit_function(function: &AssemblyFunction) -> String {
 /// # Returns
 ///
 /// A `String` representing the assembly code for the instruction.
-fn emit_instruction(instruction: &AssemblyInstruction) -> String {
+fn format_instruction(instruction: &AssemblyInstruction) -> String {
     match instruction {
         AssemblyInstruction::Mov {
             source,
             destination,
-        } => format!(
-            "\tmovl {}, {}",
-            emit_operand(source),
-            emit_operand(destination)
+        } => wrap_instruction(
+            format!(
+                "movl {}, {}",
+                format_operand(source),
+                format_operand(destination)
+            )
+            .as_str(),
         ),
         AssemblyInstruction::Ret => {
-            let mut epilogue = "\tmovq %rbp, %rsp\n".to_string();
-            epilogue.push_str("\tpopq %rbp\n");
-            epilogue.push_str("\tret\n");
+            let mut epilogue = wrap_instruction("movq %rbp, %rsp").to_string();
+            epilogue.push_str(wrap_instruction("popq %rbp").as_str());
+            epilogue.push_str(wrap_instruction("ret").as_str());
             epilogue
         }
-        AssemblyInstruction::Unary { op, operand } => {
-            format!("\t{} {}", emit_unary_op(op), emit_operand(operand))
-        }
+        AssemblyInstruction::Unary { op, operand } => wrap_instruction(
+            format!("{} {}", format_unary_operation(op), format_operand(operand)).as_str(),
+        ),
         AssemblyInstruction::AllocateStack { stack_offset } => {
-            format!("\tsubq ${}, %rsp", stack_offset)
+            wrap_instruction(format!("subq ${}, %rsp", stack_offset).as_str())
         }
     }
 }
@@ -89,7 +92,7 @@ fn emit_instruction(instruction: &AssemblyInstruction) -> String {
 /// # Returns
 ///
 /// A string representing the unary operation.
-fn emit_unary_op(op: &AssemblyUnaryOperation) -> String {
+fn format_unary_operation(op: &AssemblyUnaryOperation) -> String {
     match op {
         AssemblyUnaryOperation::Neg => "negl".to_string(),
         AssemblyUnaryOperation::Not => "notl".to_string(),
@@ -105,11 +108,11 @@ fn emit_unary_op(op: &AssemblyUnaryOperation) -> String {
 /// # Returns
 ///
 /// A `String` representing the assembly code for the operand.
-fn emit_operand(operand: &AssemblyUnaryOperand) -> String {
+fn format_operand(operand: &AssemblyUnaryOperand) -> String {
     match operand {
-        AssemblyUnaryOperand::Imm(value) => format!("${}", value),
-        AssemblyUnaryOperand::Register(register) => format!("{}", emit_register(register)),
-        AssemblyUnaryOperand::Stack(offset) => format!("{offset}(%rbp)", offset = offset),
+        AssemblyUnaryOperand::Imm(value) => format_immediate_value(value),
+        AssemblyUnaryOperand::Register(register) => format_register(register),
+        AssemblyUnaryOperand::Stack(offset) => format_stack_offset(offset),
         AssemblyUnaryOperand::Pseudo(_) => panic!(
             "Pseudo registers should not be emitted to assembly. Have you converted them correctly to actual register addresses?"
         ),
@@ -125,9 +128,62 @@ fn emit_operand(operand: &AssemblyUnaryOperand) -> String {
 /// # Returns
 ///
 /// A `String` representing the AT&T assembly syntax for the given register.
-fn emit_register(register: &AssemblyRegister) -> String {
+fn format_register(register: &AssemblyRegister) -> String {
     match register {
         AssemblyRegister::AX => "%eax".to_string(),
         AssemblyRegister::R10 => "%r10d".to_string(),
     }
+}
+
+/// Formats an integer as an immediate value string, prefixed with a dollar sign.
+///
+/// # Arguments
+///
+/// * `value` - A reference to the i32 integer to format.
+///
+/// # Returns
+///
+/// A String representing the formatted immediate value (e.g., "$123").
+fn format_immediate_value(value: &i32) -> String {
+    format!("${}", value)
+}
+
+/// Formats a stack offset as a string.
+///
+/// # Arguments
+///
+/// * `offset` - A reference to the i32 integer to format.
+///
+/// # Returns
+///
+/// A String representing the formatted stack offset (e.g., "-4(%rbp)").
+fn format_stack_offset(offset: &i32) -> String {
+    format!("{}(%rbp)", offset)
+}
+
+/// Wraps a label with a colon and newline
+///
+/// # Arguments
+///
+/// * `label`: The string slice to format.
+///
+/// # Returns
+///
+/// A `String` containing the formatted label.
+///
+fn wrap_label(label: &str) -> String {
+    format!("{}:\n", label)
+}
+
+///  Wraps an assembly instruction with a tab and newline
+///
+/// # Arguments
+///
+/// * `instruction` - The instruction string to format.
+///
+/// # Returns
+///
+/// A formatted string with a tab at the beginning and a newline at the end.
+fn wrap_instruction(instruction: &str) -> String {
+    format!("\t{}\n", instruction)
 }
