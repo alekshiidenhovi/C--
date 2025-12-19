a040582b30521b8e8c66b0444bd64aec52b20d11
Aleks Hiidenhovi <hiidenhovi.aleks@gmail.com>
2025-12-19 18:22:30 +0200

    replace single character parsers with generic parser generator

--- src/compiler/lexer/mod.rs
+++ src/compiler/lexer/mod.rs
@@ -61,17 +61,17 @@ pub fn tokenize(input_str: &str) -> Vec<Token> {
         Box::new(parse_identifier_or_keyword),
         Box::new(parse_constant),
         create_regex_parser(Regex::new(r"^--").unwrap(), Token::DoubleHyphen),
-        Box::new(create_character_parser('-', Token::Hyphen)),
-        Box::new(create_character_parser('~', Token::Tilde)),
-        Box::new(create_character_parser('(', Token::OpenParen)),
-        Box::new(create_character_parser(')', Token::CloseParen)),
-        Box::new(create_character_parser('{', Token::OpenBrace)),
-        Box::new(create_character_parser('}', Token::CloseBrace)),
-        Box::new(create_character_parser(';', Token::Semicolon)),
-        Box::new(create_character_parser('+', Token::Plus)),
-        Box::new(create_character_parser('*', Token::Asterisk)),
-        Box::new(create_character_parser('/', Token::ForwardSlash)),
-        Box::new(create_character_parser('%', Token::Percent)),
+        create_regex_parser(Regex::new(r"^\-").unwrap(), Token::Hyphen),
+        create_regex_parser(Regex::new(r"^\~").unwrap(), Token::Tilde),
+        create_regex_parser(Regex::new(r"^\(").unwrap(), Token::OpenParen),
+        create_regex_parser(Regex::new(r"^\)").unwrap(), Token::CloseParen),
+        create_regex_parser(Regex::new(r"^\{").unwrap(), Token::OpenBrace),
+        create_regex_parser(Regex::new(r"^\}").unwrap(), Token::CloseBrace),
+        create_regex_parser(Regex::new(r"^\;").unwrap(), Token::Semicolon),
+        create_regex_parser(Regex::new(r"^\+").unwrap(), Token::Plus),
+        create_regex_parser(Regex::new(r"^\*").unwrap(), Token::Asterisk),
+        create_regex_parser(Regex::new(r"^\/").unwrap(), Token::ForwardSlash),
+        create_regex_parser(Regex::new(r"^\%").unwrap(), Token::Percent),
     ];
     loop {
         string_stream = string_stream.trim_start().to_string();
@@ -179,44 +179,6 @@ fn parse_constant(input_str: &str) -> LexerParseResult<Token> {
     }
 }
 
-/// Attempts to parse a single character from the input string.
-///
-/// # Arguments
-///
-/// * `input_str`: The input string to parse.
-/// * `target_char`: The character to match.
-/// * `parsed_token`: The token to return if the character is matched
-///
-/// # Returns
-///
-/// On successful parsing, return a tuple of remaining input string and the parsed character.
-/// On failure, returns an unexpected character error.
-fn create_character_parser(target_char: char, parsed_token: Token) -> LexerParser {
-    Box::new(move |input_str: &str| {
-        if input_str.is_empty() {
-            return Err(LexerError::EmptyInputString);
-        }
-        let (next_char, rest) = split_first_char(&input_str).unwrap();
-        if next_char == target_char {
-            Ok((rest.to_string(), parsed_token.clone()))
-        } else {
-            Err(LexerError::UnexpectedCharacter {
-                found: next_char,
-                expected: target_char,
-            })
-        }
-    })
-}
-
-fn split_first_char(s: &str) -> Option<(char, &str)> {
-    let mut chars = s.chars();
-    if let Some(first_char) = chars.next() {
-        let rest_of_str = chars.as_str();
-        return Some((first_char, rest_of_str));
-    }
-    None
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -254,39 +216,21 @@ mod tests {
     }
 
     #[test]
-    fn parse_character_matching_character() {
-        let input = "abc";
-        let parser = create_character_parser('a', Token::Identifier("a".to_string()));
+    fn parse_valid_single_hyphen() {
+        let input = "-a";
+        let parser = create_regex_parser(Regex::new(r"^-").unwrap(), Token::Hyphen);
         let result = parser(input);
         assert!(result.is_ok());
-        assert_eq!(
-            result.unwrap(),
-            (String::from("bc"), Token::Identifier("a".to_string()))
-        );
-    }
-
-    #[test]
-    fn parse_character_nonmatching_character() {
-        let input = "abc";
-        let parser = create_character_parser('b', Token::Identifier("b".to_string()));
-        let result = parser(input);
-        assert!(result.is_err());
-        assert_eq!(
-            result.unwrap_err(),
-            LexerError::UnexpectedCharacter {
-                found: 'a',
-                expected: 'b'
-            }
-        );
+        assert_eq!(result.unwrap(), (String::from("a"), Token::Hyphen));
     }
 
     #[test]
     fn parse_valid_double_hyphen() {
-        let input = "--";
+        let input = "--a";
         let parser = create_regex_parser(Regex::new(r"^--").unwrap(), Token::DoubleHyphen);
         let result = parser(input);
         assert!(result.is_ok());
-        assert_eq!(result.unwrap(), (String::from(""), Token::DoubleHyphen));
+        assert_eq!(result.unwrap(), (String::from("a"), Token::DoubleHyphen));
     }
 
     #[test]
